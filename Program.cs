b9a6d2d7aabd98160763fa5209b9c17dacadfad6
Arwezom <jakob.smogawetz@gmail.com>
2024-04-16 14:48:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stegosaurus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
